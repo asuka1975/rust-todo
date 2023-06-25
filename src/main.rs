1cbbe7ed83c4f326f022d253cb603562255acb40
hungr <hungryfactory@icloud.com>
2023-06-25 21:21:28 +0900

    chore: introduce web framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
