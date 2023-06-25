08420de2d98d6149de98c2eeeafddf942687c38f
hungr <hungryfactory@icloud.com>
2023-06-25 21:49:00 +0900

    feat: intialize web page

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{
+    get, web, middleware,
+    http::{StatusCode},
+    App, HttpServer, Responder, HttpResponse, Result
+};
+use actix_files::{Files};
 
 #[get("/")]
-async fn index() -> impl Responder {
-    "Hello world!"
+async fn index() -> Result<HttpResponse> {
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../html/index.html")))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(index)
+        App::new()
+            .wrap(middleware::Compress::default())
+            .service(Files::new("/static", "./html/static").show_files_listing())
+            .service(index)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
